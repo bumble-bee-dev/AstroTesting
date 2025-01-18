508a787d672b56e0060a894cf6ffc34ab99d3558
Charlie Houston <charlie@quantumelephant.co.uk>
2025-01-18 13:35:00 +0000

    Attempt 4 to get images to work when deployed to GItHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bumble-bee-dev.github.io',
-    build: {
-        assets: 'assets/',
-    }
+    base: "/AstroTesting/",
 })
